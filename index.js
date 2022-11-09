b1bbf145378ed28faae7e76f46782295e674f5fc
dom alexander <dom.alexan@gmail.com>
2022-11-09 15:11:17 -0700

    commit tree 46394471d209ddc382cc8050852e14446fb31aa7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.querySelector('main');
+main.remove();
+
+document.getElementById('body');
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Freddy Mercury is the champion!"
+body.append('h1')
